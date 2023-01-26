6fa1d827f556164eb8a32e53638b931da91f11f6
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-01-26 15:44:16 +0000

    chore: add some text to homepage

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Homepage = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1>Hey, I'm Michael.</h1>
+      <p>
+        I am an aspiring Full Stack Developer based in Sandy, UK. 
+        In my spare time you'll find me playing the piano, exploring the outdoors, or playing video games.
+        <br />
+        <br />
+        Having recently completed a 12-week intensive coding bootcamp, I am now looking for my first role as a developer.
+      </p>
     </div>
   );
 };
