1e3f0ca584d7d1aab1519f1ebb573d61a50a49b8
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-01-29 15:42:28 +0000

    create navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleClass = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <nav className="navbar is-transparent is-spaced">
+            <div className="navbar-brand">
+                <a role="button" className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navMenu" onClick={toggleClass}>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div id="navMenu" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+                <div className="navbar-end">
+                    <Link to="/" className="navbar-item is-size-5 is-family-secondary has-text-weight-semibold">Home</Link>
+                    <Link to="/about" className="navbar-item is-size-5 is-family-secondary has-text-weight-semibold">About</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import App from './App'
 import './index.sass'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
