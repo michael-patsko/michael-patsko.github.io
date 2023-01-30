f635b98903d76245bff9188901a42e363b531a3a
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-01-30 17:15:22 +0000

    move navbar styling to its own file

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./navbar.sass";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -10,9 +11,12 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="navbar is-transparent is-spaced">
+        <nav className="navbar is-spaced">
             <div className="navbar-brand">
-                <p className="navbar-item navbar-name is-uppercase is-size-5-tablet is-size-6-mobile is-family-secondary has-text-weight-bold">Michael Patsko</p>
+                <div className="navbar-item">
+                    <img className="image navbar-logo"src="src\assets\placeholder.png" width="48" height="48"></img>
+                    <p className="navbar-name is-uppercase is-size-5-tablet is-size-6-mobile is-family-secondary has-text-weight-bold">Michael Patsko</p>
+                </div>
                 <a role="button" className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navMenu" onClick={toggleClass}>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
