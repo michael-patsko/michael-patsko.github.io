9f5910e24355defc97aac632ff52440b3bff0f48
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-01-30 15:59:20 +0000

    modify responsive text size on homepage

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -5,9 +5,8 @@ const Homepage = () => {
   return (<>
     <div className="section">
       <div className="content">
-        <h1 className="title is-size-1-desktop is-size-3-mobile">Hey, I'm Michael.</h1>
-        <h3 className="subtitle is-size-4-desktop is-size-6-mobile has-text-weight-light">
-          <br/>
+        <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">Hey, I'm Michael.</h1>
+        <h3 className="subtitle is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-light">
           I'm an aspiring Full Stack Developer based in Sandy, UK. 
           <br/><br/>
           Having recently completed a 12-week intensive coding bootcamp, I am now looking for my first role as a junior developer.
