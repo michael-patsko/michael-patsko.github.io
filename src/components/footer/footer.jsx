b63a79b4ac071e13a049c9b1ce8665dd2b9f9eb4
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-02-01 17:29:58 +0000

    update footer content and styling

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,20 +6,37 @@ const Footer = () => {
   <hr className="footer-separator"/>
     <footer className="footer">
       <div className="level">
+
         <div className="level-item">
           <div className="footer-text">
             <h3 className="title is-size-7 is-family-secondary has-text-weight-semibold">Michael Patsko</h3>
+            <br className="is-hidden-mobile"/>
+            <br className="is-hidden-mobile"/>
           </div>
         </div>
+
         <div className="level-item">
           <div className="footer-text has-text-centered-mobile">
             <h3 className="is-size-7 is-family-secondary has-text-weight-semibold">Social</h3>
             <p className="is-size-7 is-family-secondary has-text-weight-light">
-              <a className="is-underlined" href="https://github.com/michael-patsko" target="_blank" rel="noopener noreferrer">Github</a> |{" "}
+              <a className="is-underlined" href="https://github.com/michael-patsko" target="_blank" rel="noopener noreferrer">Github</a>
+              <br/>
               <a className="is-underlined" href="https://www.linkedin.com/in/michael-patsko-871086195/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </p>
           </div>
         </div>
+
+        <div className="level-item">
+          <div className="footer-text has-text-centered-mobile">
+            <h3 className="is-size-7 is-family-secondary has-text-weight-semibold">Legal</h3>
+            <p className="is-size-7 is-family-secondary has-text-weight-light">
+              <a className="is-underlined" href="https://scripts.sil.org/cms/scripts/page.php?item_id=OFL-FAQ_web" target="_blank" rel="noopener noreferrer">Lora - Font License</a>
+              <br/>
+              <a className="is-underlined" href="https://scripts.sil.org/cms/scripts/page.php?item_id=OFL-FAQ_web" target="_blank" rel="noopener noreferrer">Raleway - Font License</a>
+            </p>
+          </div>
+        </div>
+
       </div>
     </footer>
     </>);
