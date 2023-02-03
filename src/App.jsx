fe06f6f833bc36c87538f67b76bae5c60fb76ef0
Michael Patsko <michael_patsko@yahoo.co.uk>
2023-02-03 13:43:00 +0000

    update dark mode toggle icon and comment homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.sass'
 import HomePage from './components/pages/HomePage'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSun, faMoon, fas } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-
+  library.add(faSun, faMoon, fas)
   return (
     <div className="App">
       <HomePage />
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,20 +2,32 @@ import React from "react";
 import "./navbar.sass";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Navbar = () => {
-    const [isActive, setIsActive] = useState(false);
-    const defaultScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const [isDarkMode, setIsDarkMode] = useState(defaultScheme);
 
+    // This toggles the navbar menu between active and inactive
+    const [isActive, setIsActive] = useState(false);
     const toggleClass = () => {
         setIsActive(!isActive);
     };
 
+    // This is used to detect the default theme of the user's device
+    const defaultScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    // This toggles the theme between dark and light mode
+    const [isDarkMode, setIsDarkMode] = useState(defaultScheme);
     const toggleDarkMode = () => {
         setIsDarkMode(!isDarkMode);
     };
 
+    // This sets the theme of the page to the default theme of the user's device on page load
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", `${defaultScheme ? "dark" : "light"}`);
+    }, []);
+
+    // This updates the theme of the page when the user toggles the theme
     useEffect(() => {
         if (isDarkMode) {
             document.documentElement.setAttribute("data-theme", "dark");
@@ -24,9 +36,13 @@ const Navbar = () => {
         }
     }, [isDarkMode]);
 
+    const [iconsLoaded, setIconsLoaded] = useState(false);
+
     useEffect(() => {
-        document.documentElement.setAttribute("data-theme", `${defaultScheme ? "dark" : "light"}`);
-    }, []);
+        if (library.definitions.fas.sun && library.definitions.fas.moon) {
+            setIconsLoaded(true);
+        }
+    }, [library.definitions.fas]);
 
     return (
         <nav className="navbar is-spaced">
@@ -37,9 +53,12 @@ const Navbar = () => {
                 </div>
 
                 <div className="navbar-item">
-                    <div className="button" onClick={toggleDarkMode}>
-                        <span className="is-family-secondary">Toggle</span>
-                    </div>
+                    <span className={`icon is-large-tablet is-medium-mobile theme-toggle fa-2x fa-fw`} onClick={toggleDarkMode}>
+                        {iconsLoaded && <FontAwesomeIcon 
+                            className="theme-toggle-icon" 
+                            icon={`fa-solid fa-${isDarkMode ? "sun" : "moon"}`}
+                        />}
+                    </span>
                 </div>
 
                 <a role="button" className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navMenu" onClick={toggleClass}>
